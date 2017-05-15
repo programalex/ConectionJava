21b4f39f674cd5bebaa405860746700499e7f90f
Alex Velez <pensalex@gmail.com>
2017-05-15 11:31:28 -0500

    desde neatbeans cambios

--- src/interfaces/probarConexion.java
+++ src/interfaces/probarConexion.java
@@ -1,7 +1,8 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
- * and open the template in the editor.desde visualcode
+ * and open the template in the editor.desde visualcode;a
+dfdsfsdfsdfsdf
  */
 package interfaces;
 
